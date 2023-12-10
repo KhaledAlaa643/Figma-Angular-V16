cfd27e57c8434d47140810a51ae4ca3df3c7097e
KhaledAlaa643 <khaledalaaka643@gmail.com>
2023-12-10 16:58:43 +0200

    routing

--- src/app/Ui/ui-routing.module.ts
+++ src/app/Ui/ui-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-    {path: "home", component:DashboardComponent}
+    {path: "", component:DashboardComponent}
 
 ];
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './Ui/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: "home", component:DashboardComponent}
 ];
 
 @NgModule({
